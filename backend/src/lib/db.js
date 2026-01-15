b383efb5f573204a2199da776801c64a64c14033
manish <manishsuriyal21@gmail.com>
2026-01-15 18:55:35 +0530

    feat: integrate Inngest for user synchronization and deletion
    - Added Inngest dependency to package.json.
    - Created Inngest functions for syncing and deleting users based on Clerk events.
    - Implemented database connection checks in db.js.
    - Updated env.js to include new environment variables for Inngest and Stream API.
    - Created User model to store user data.
    - Configured Express server to use Inngest functions and CORS middleware.

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,9 @@ import {ENV} from './env.js';
 
 export const connectDB = async() => {
  try {
+   if(!ENV.DB_URL){
+      throw new Error ("db url is not defiend in env")
+   }
     const connect = await mongoose.connect(ENV.DB_URL)
     console.log("Connect to db",connect.connection.host)
  } catch (error) {
